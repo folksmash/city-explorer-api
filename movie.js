87b84f96ddc2a0ac1358d78439e073dc3fab5a64
Jordan Folk <jordan.folk7@gmail.com>
2021-11-06 18:40:57 -0700

    added lab 07 server functions

---
+++ movie.js
@@ -0,0 +1,37 @@
+'use strict'
+require('dotenv').config();
+const axios = require('axios');
+
+const movieAPI = process.env.MOVIE_API_KEY;
+
+class Movies {
+    constructor(obj) {
+        this.title = obj.title;
+        this.overview = obj.overview;
+        this.image_url = `https://image.tmdb.org/t/p/w500${obj.poster_path}`;
+        this.popularity = obj.popularity;
+        this.release_date = obj.release_date;
+    }
+}
+
+let getMovies = async (req, res) => {
+
+    let movieName = req.query.query;
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${movieAPI}&query=${movieName}&page=1`;
+    
+    try {
+        let result = await axios(url);
+        let movieData = result.data.results.map(movie => new Movies(movie));
+        console.log(movieData);
+        if(movieData) {
+            res.status(200).send(movieData)
+        } else {
+            res.status(404).send('nonfound')
+        }
+    } catch {
+        return res.status(500).send('internal error');
+    }
+}
+
+module.exports = getMovies;
--- server.js
+++ server.js
@@ -3,15 +3,45 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// const getWeather = require('./getWeather')
-
+const weather = require('./data/weather.json');
+const axios = require('axios')
 const app = express();
+const getMovies = require('./movie');
 
 app.use(cors());
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT
 
-// app.get('/weather', getWeather);
+const getWeather = async (req, res) => { 
+    const lat = req.query.lat;
+    const lon = req.query.lon;
+    const searchQuery = req.query.searchQuery;
+    console.log(weather);
+try {
+    // const cityLookingfor = weather.find(city => searchQuery === city.city_name);
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily/?key=${process.env.WEATHER_API_KEY}&lang=en&lat=${lat}&lon=${lon}&days=5`;
+    console.log(url);
+    const cityLookingfor = await axios.get(url)
+    console.log(cityLookingfor);
+    const weatherArray = cityLookingfor.data.data.map(day => new Forecast(day));
+    res.send(weatherArray);
+    } catch(error) {
+        console.log(error);
+    } 
+};
+
+function Forecast(day) {
+    this.date = day.valid_date;
+    this.description = day.weather.description;
+}
+
+
+app.get('/movie', getMovies);
+app.get('/weather', getWeather);
 app.get('/*', (req, res) => res.status(404).send('not found'))
+
+
+
+
 
 app.listen(PORT, () => console.log(`I am a server that is listening on port:${PORT}`));
